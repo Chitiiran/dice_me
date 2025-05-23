aeffc78e900be2e4dd737ccc627a684df493b260
Chitiiran <chitii91@gmail.com>
2025-05-22 20:33:04 -0400

    app label and keys

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -1,3 +1,6 @@
+import java.io.FileInputStream // Add this import
+import java.util.Properties // Add this import
+
 plugins {
     id("com.android.application")
     id("kotlin-android")
@@ -5,8 +8,17 @@ plugins {
     id("dev.flutter.flutter-gradle-plugin")
 }
 
+// --- START: Keystore properties loading (Kotlin DSL) ---
+val keystoreProperties = Properties() // Removed java.util. because of import
+val keystorePropertiesFile = rootProject.file("key.properties")
+if (keystorePropertiesFile.exists()) {
+    keystorePropertiesFile.inputStream().use { keystoreProperties.load(it) }
+}
+// --- END: Keystore properties loading (Kotlin DSL) ---
+
+
 android {
-    namespace = "com.example.dice_me"
+    namespace = "com.example.dice_me" // Make sure this matches your applicationId
     compileSdk = flutter.compileSdkVersion
     ndkVersion = flutter.ndkVersion
 
@@ -19,9 +31,20 @@ android {
         jvmTarget = JavaVersion.VERSION_11.toString()
     }
 
+    // --- START: Signing Configs (Kotlin DSL) ---
+    signingConfigs {
+        create("release") {
+            storeFile = file(keystoreProperties.getProperty("storeFile"))
+            storePassword = keystoreProperties.getProperty("storePassword")
+            keyAlias = keystoreProperties.getProperty("keyAlias")
+            keyPassword = keystoreProperties.getProperty("keyPassword")
+        }
+    }
+    // --- END: Signing Configs (Kotlin DSL) ---
+
     defaultConfig {
         // TODO: Specify your own unique Application ID (https://developer.android.com/studio/build/application-id.html).
-        applicationId = "com.example.dice_me"
+        applicationId = "dev.ckm.dice_me" // This is where you set your unique package name
         // You can update the following values to match your application needs.
         // For more information, see: https://flutter.dev/to/review-gradle-config.
         minSdk = flutter.minSdkVersion
@@ -32,9 +55,12 @@ android {
 
     buildTypes {
         release {
-            // TODO: Add your own signing config for the release build.
-            // Signing with the debug keys for now, so `flutter run --release` works.
-            signingConfig = signingConfigs.getByName("debug")
+            // Apply your new release signing config
+            signingConfig = signingConfigs.getByName("release")
+
+            // Recommended for release builds:
+            isMinifyEnabled = true // Corrected syntax
+            isShrinkResources = true // Corrected syntax
         }
     }
 }
